5e6982984127690c8f16b8c4f86314c4387a5b59
Ishaan <ishaanmalikrm7@gmail.com>
2020-09-02 15:28:37 +0530

    changes

---
+++ src/components/Conference.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Dev from "../assets/Dev.jpg";
+import Card from "@material-ui/core/Card";
+import styles from "../index.css";
+
+const DEFAULT_PLACEHOLDER_IMAGE = Dev;
+
+const Conference = ({ conference }) => {
+  let image = conference.imageURL.includes(
+    '"https://storage.googleapis.com/konfhub-bd9c9.appspot.com/'
+  )
+    ? DEFAULT_PLACEHOLDER_IMAGE
+    : conference.imageURL;
+  return (
+    <div style={{ margin: "20px" }}>
+      <Card
+        style={{
+          width: "300px",
+          height: "450px",
+          boxShadow: "5px 5px 5px 3px black",
+        }}
+      >
+        <h2 style={{ display: "flex", justifyContent: "center" }}>
+          {conference.confName}
+        </h2>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img
+            width="200px"
+            height="150px"
+            alt={`Conference Name: ${conference.confName}`}
+            src={image}
+          />
+        </div>
+        <p style={{ display: "flex", justifyContent: "center" }}>
+          {conference.confStartDate} ({conference.entryType})
+        </p>
+        <p style={{ display: "flex", justifyContent: "center" }}>
+          {conference.country} ({conference.city})
+        </p>
+        <a
+          style={{ display: "flex", justifyContent: "center" }}
+          href={conference.confUrl}
+        >
+          Visit Conf Website
+        </a>
+      </Card>
+    </div>
+  );
+};
+
+export default Conference;
