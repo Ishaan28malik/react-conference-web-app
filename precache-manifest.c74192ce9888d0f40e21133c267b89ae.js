96596fba87a5b8e27347b28c4a5d1b361d1090a2
Ishaan <ishaanmalikrm7@gmail.com>
2020-09-02 15:27:24 +0530

    Updates

---
+++ precache-manifest.c74192ce9888d0f40e21133c267b89ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6679e37edb843b848478d2982ee041",
+    "url": "/react-conference-web-app/index.html"
+  },
+  {
+    "revision": "973b64a8b36495e83b7b",
+    "url": "/react-conference-web-app/static/css/main.6e1a9b75.chunk.css"
+  },
+  {
+    "revision": "91bdad32600eaaaecf2e",
+    "url": "/react-conference-web-app/static/js/2.f3f0718b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-conference-web-app/static/js/2.f3f0718b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973b64a8b36495e83b7b",
+    "url": "/react-conference-web-app/static/js/main.743343fd.chunk.js"
+  },
+  {
+    "revision": "89a9acba430ccbc67eaf",
+    "url": "/react-conference-web-app/static/js/runtime-main.02aabd97.js"
+  },
+  {
+    "revision": "abcf83f7a18eda4a5d4d9e6403b39052",
+    "url": "/react-conference-web-app/static/media/Dev.abcf83f7.jpg"
+  }
+]);
