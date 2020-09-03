ab624fa074999d714c88583569666c635a0045b8
Ishaan <ishaanmalikrm7@gmail.com>
2020-09-03 17:50:56 +0530

    Updates

--- precache-manifest.a7d55f3737545f12806730da2d8a45d9.js
+++ precache-manifest.a7d55f3737545f12806730da2d8a45d9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eed84953d7a5ade1e4ec15a9b6c30496",
+    "revision": "7a6db6a0326f7fcd13857326a3afd20f",
     "url": "/react-conference-web-app/index.html"
   },
   {
-    "revision": "a75156b0eecb886a6b80",
+    "revision": "9845a24dd109e322b3d3",
     "url": "/react-conference-web-app/static/css/main.6e1a9b75.chunk.css"
   },
   {
-    "revision": "91bdad32600eaaaecf2e",
-    "url": "/react-conference-web-app/static/js/2.f3f0718b.chunk.js"
+    "revision": "2ab82a03cdfde368e636",
+    "url": "/react-conference-web-app/static/js/2.3853e683.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/react-conference-web-app/static/js/2.f3f0718b.chunk.js.LICENSE.txt"
+    "url": "/react-conference-web-app/static/js/2.3853e683.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a75156b0eecb886a6b80",
-    "url": "/react-conference-web-app/static/js/main.50a1ac22.chunk.js"
+    "revision": "9845a24dd109e322b3d3",
+    "url": "/react-conference-web-app/static/js/main.4689b8c9.chunk.js"
   },
   {
     "revision": "89a9acba430ccbc67eaf",
